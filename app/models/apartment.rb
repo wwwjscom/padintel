6f3583a81a2efa963887c81defcb9c3629ec9c0d
Jason Soo <wwwjscom@gmail.com>
2011-10-28 16:54:28 -0400

    Wrong order...

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,9 +3,9 @@ class Apartment < ActiveRecord::Base
 	
 	def self.find_with_features(required_features, desired_features, nots_features)
     apts = Apartment.all
-		unless required_features.empty? apts.delete_if {|apt| (apt.features & required_features).size < required_features.size}
-    unless nots_features.empty? apts.delete_if {|apt| (apt.features - nots_features).size < apt.features.size}
-    unless desired_features.empty? apts.delete_if {|apt| (apt.features - desired_features).size == apt.features.size}
+      apts.delete_if {|apt| (apt.features & required_features).size < required_features.size} unless required_features.empty? 
+      apts.delete_if {|apt| (apt.features - nots_features).size < apt.features.size} unless nots_features.empty? 
+      apts.delete_if {|apt| (apt.features - desired_features).size == apt.features.size} unless desired_features.empty? 
     apts
 	end
 end
