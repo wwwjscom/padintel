9a1e9c302aa36fa69b4ac0e71d9de19bf8ebb670
Jason Soo <wwwjscom@gmail.com>
2011-10-28 17:44:41 -0400

    Looking better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Padintel::Application.routes.draw do
   resources :sherlocks
 
+  match 'about' => 'sherlock#about', :as => :about
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +52,7 @@ Padintel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "sherlocks#new"
 
   # See how all your routes lay out with "rake routes"
 
