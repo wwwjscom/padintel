f06c3f37e488a438ea67354d97c85b0482be8281
Jason Soo <wwwjscom@gmail.com>
2011-10-27 23:38:28 -0400

    debug

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,9 +3,13 @@ class Apartment < ActiveRecord::Base
 	
 	def self.find_with_features(required_features, desired_features, nots_features)
     apts = Apartment.all
+    puts "1"
 		apts.delete_if {|apt| (apt.features & required_features).size < required_features.size}
+    puts "2"
     apts.delete_if {|apt| (apt.features - nots_features).size < apt.features.size}
+    puts "3"
     apts.delete_if {|apt| (apt.features - desired_features).size == apt.features.size}
+    puts "4"
     apts
 	end
 end
