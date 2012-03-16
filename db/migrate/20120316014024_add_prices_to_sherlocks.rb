9925801b6c819793c9f2e6351704bc97963680df
Jason Soo <wwwjscom@gmail.com>
2012-03-16 01:16:03 -0400

    Fixed a bug that caused all the old sherlocks to die off

--- db/migrate/20120316014024_add_prices_to_sherlocks.rb
+++ db/migrate/20120316014024_add_prices_to_sherlocks.rb
@@ -2,6 +2,13 @@ class AddPricesToSherlocks < ActiveRecord::Migration
   def self.up
     add_column :sherlocks, :price_max, :integer, :default => 0
     add_column :sherlocks, :price_min, :integer, :default => 0
+    
+    # Go through and update the old sherlocks to have the proper
+    # default price values, instead of nils, which will cause errors
+    Sherlock.where(:price_max => nil).each do |a|
+      a.update_attribute(:price_max, 0)
+      a.update_attribute(:price_min, 0)
+    end
   end
 
   def self.down
